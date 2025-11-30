3bda2c50b042bdbf24886e09baae086d5b701480
Kamrul Hasan <kakamrul2000@gmail.com>
2025-12-01 03:37:46 +0600

    test3

--- lib/music_player_screen.dart
+++ lib/music_player_screen.dart
@@ -11,9 +11,95 @@ class MusicPlayerScreen extends StatefulWidget {
 
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
   final AudioPlayer _audioPlayer = AudioPlayer();
-  final List<Song> _playlist = [];
+  final List<Song> _playlist = [
+    Song(
+      songName: 'sample1',
+      artiestName: 'No Name',
+      songUrl: '//samplelib.com/lib/preview/mp3/sample-3s.mp3',
+      durationSecond: 3,
+    ),
+    Song(
+      songName: 'sample2',
+      artiestName: 'No Name',
+      songUrl: '//samplelib.com/lib/preview/mp3/sample-3s.mp3',
+      durationSecond: 3,
+    ),
+    Song(
+      songName: 'sample3',
+      artiestName: 'No Name',
+      songUrl: '//samplelib.com/lib/preview/mp3/sample-3s.mp3',
+      durationSecond: 3,
+    ),
+    Song(
+      songName: 'sample4',
+      artiestName: 'No Name',
+      songUrl: '//samplelib.com/lib/preview/mp3/sample-3s.mp3',
+      durationSecond: 3,
+    ),
+  ];
+  int _currentIndex = 0;
+  bool _isPlaying = false;
+  Duration _duration = Duration.zero;
+  Duration _position = Duration.zero;
+
+  @override
+  void initState() {
+    _listenToPlayer();
+    super.initState();
+  }
+
+  void _listenToPlayer() {
+    _audioPlayer.onDurationChanged.listen((duration) {
+      setState(() => _duration = duration);
+    });
+    _audioPlayer.onPositionChanged.listen((position) {
+      setState(() => _position = position);
+    });
+    _audioPlayer.onPlayerStateChanged.listen((state) {
+      setState(() => _isPlaying = state == PlayerState.playing);
+    });
+    _audioPlayer.onPlayerComplete.listen((_) => next());
+  }
+
+  Future<void> playSong(int index) async {
+    _currentIndex = index;
+    final song = _playlist[index];
+    setState(() {
+      _position = Duration.zero;
+      _duration = Duration(seconds: song.durationSecond);
+    });
+    await _audioPlayer.stop();
+    await _audioPlayer.play(UrlSource(_playlist[index].songUrl));
+  }
+
+  Future<void> next() async {
+    final int next = (_currentIndex + 1) % _playlist.length;
+    await playSong(next);
+  }
+
+  Future<void> previous() async {
+    final int previous =
+        (_currentIndex - 1 + _playlist.length) % _playlist.length;
+    await playSong(previous);
+  }
+
+  Future<void> _togglePlayer() async {
+    if (_isPlaying) {
+      await _audioPlayer.pause();
+    } else {
+      await _audioPlayer.resume();
+    }
+  }
+
+  String formatDuration(Duration duration) {
+    final int minutes = duration.inMinutes;
+    final int second = duration.inSeconds.remainder(60);
+    return '$minutes: ${second.toString().padLeft(2, '0')}';
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Song song = _playlist[_currentIndex];
     return Scaffold(
       appBar: AppBar(title: Center(child: Text('Music App'))),
       body: Column(
@@ -47,13 +133,15 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: 10,
+              itemCount: _playlist.length,
               itemBuilder: (context, index) {
+                final Song song = _playlist[index];
                 return ListTile(
-                  title: Text('song name'),
-                  subtitle: Text('artiest name'),
-                  leading: Text('1'),
+                  title: Text(song.songName),
+                  subtitle: Text(song.artiestName),
+                  leading: Text("${_playlist[index]}"),
                   trailing: Icon(Icons.skip_next),
+                  onTap: () => playSong(index),
                 );
               },
             ),
